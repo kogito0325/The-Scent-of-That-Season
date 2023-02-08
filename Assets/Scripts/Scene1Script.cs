31dc82729263d50ebddd1e4aab38f6bc85f4cd2c
kogito0325 <qasw405@gmail.com>
2023-02-08 10:50:18 +0900

    (수정) 대사 출력 방식 변경
    엑셀 시트로 읽은 후 인덱스로 관리

---
+++ Assets/Scripts/Scene1Script.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene1Script : MonoBehaviour
+{
+    public Image emptyImage; //기존이미지 : 유가현
+    public Sprite[] characterImages;
+
+    public Text ChatText; // 대사
+    public Text CharacterName; //캐릭터 이름
+
+    public string writerText = "";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TextPractice());
+    }
+
+    public IEnumerator NormalChat(string narrator, string narration, int Ch2)
+    { 
+        CharacterName.text = narrator;
+        writerText = "";
+        emptyImage.sprite = characterImages[Ch2];
+
+        for (int i = 0; i < narration.Length; i++)
+        {
+            writerText += narration[i];
+            ChatText.text = writerText;
+            yield return new WaitForSeconds(.02f);
+        }
+
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                break;
+            }
+            yield return null;
+        }
+    }
+    IEnumerator TextPractice()
+    {
+        List<Dictionary<string, object>> data_Dialog = CSVReader.Read("TestTable");
+        List<Dictionary<string, object>> name_Dialog = CSVReader.Read("CharacterNameTable");
+        string str1 = PlayerPrefs.GetString("Name1");
+        string str2 = PlayerPrefs.GetString("Name2");
+        string name = str1 + str2;
+        
+        for (int i = 0; i < data_Dialog.Count; i++)
+        {
+            int nameIdx = int.Parse(data_Dialog[i]["CHARACTER"].ToString());
+            name = name_Dialog[nameIdx]["NAME"].ToString();
+            if (name == "주인공")
+                name = str1 + str2;
+            yield return StartCoroutine(NormalChat(name, data_Dialog[i]["SCRIPT"].ToString(), int.Parse(data_Dialog[i]["CHARACTER"].ToString())));
+            Debug.Log(data_Dialog[i]["SCRIPT"].ToString());
+        }
+        yield return StartCoroutine(NormalChat(str1 + str2, "날씨가 쌀쌀해졌네..", 0));
+        yield return StartCoroutine(NormalChat("유가현", "안녕, " + str1 + str2 + "! 오늘 날씨가 춥지?",2));
+        yield return StartCoroutine(NormalChat("유가현", str2 + " 진짜 춥다. 그치?",2));
+        yield return StartCoroutine(NormalChat("유가현", "(진짜 졸라 춥네)",2));
+        yield return StartCoroutine(NormalChat("지구하", "이게 뭐가 추워!",1));
+        yield return StartCoroutine(NormalChat("지구하", "하나도 안춥지" + str2 + "?",1));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
