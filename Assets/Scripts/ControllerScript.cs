ea2a50a7cb6b93f999abb7aa0285194085d34e48
NamGeuna <rmsdk3419@naver.com>
2023-03-25 04:10:36 +0900

    (추가)내가 싼 똥 이부건이 치우기 | 알바콘텐츠 주인공 스프라이트 애니메이션 추가

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class ControllerScript : MonoBehaviour
 {
-    float moveX, moveY;
+    public float moveSpeed; //Player Speed
 
-    [Header("이동속도 조절")]
-    [SerializeField][Range(1f, 30f)] float moveSpeed = 20f;
-
+    private Animator anim; // Animator 변수 불러오기
+    void Start()
+    {
+        anim = GetComponent<Animator>(); //anim 변수 선언     
+    }
     void Update()
     {
-        //이동키: WASD, 상하좌우 이동
-        moveX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        moveY = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        if(Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0f) // Left, Right Move
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            anim.SetBool("notmove", false);
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f) // Up, Down Move
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            anim.SetBool("notmove", false);
+        }
+        else
+        {
+            anim.SetBool("notmove", true);
+        }
 
-        transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
-
+        // Animation Move X, Move Y
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
     }
+
 }
+
+
+    //float moveX, moveY;
+
+    //[Header("이동속도 조절")]
+    //[SerializeField][Range(1f, 30f)] float moveSpeed = 20f;
+
+    //void Update()
+    //{
+        //이동키: WASD, 상하좌우 이동
+    //  moveX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+    //    moveY = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+    //    transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
+    //}
