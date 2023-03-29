2af884b9bb8354f0c90f6de6c5b10484f6324993
NamGeuna <rmsdk3419@naver.com>
2023-03-30 02:23:09 +0900

    (추가 및 수정)애니메이션, 음식콜라이더

---
+++ Assets/Scripts/ContentInventory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentInventory : MonoBehaviour //변수값 저장할때 쓸거
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
